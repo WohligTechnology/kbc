1e23f89fb47e6788f226287d250c69eb3a1db043
TusharSachde <tushar@wohlig.com>
2016-03-17 14:06:05 +0530

    Welcome & Thank you

--- js/app.js
+++ js/app.js
@@ -17,22 +17,10 @@ firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, cfpL
 
   $stateProvider
 
-  .state('intro', {
+    .state('intro', {
     url: "/intro",
     templateUrl: "views/template.html",
     controller: 'IntroCtrl'
-  })
-
-  .state('wish', {
-    url: "/wish",
-    templateUrl: "views/template.html",
-    controller: 'WishCtrl'
-  })
-
-  .state('work', {
-    url: "/work",
-    templateUrl: "views/template.html",
-    controller: 'WorkCtrl'
   })
 
   .state('playingWithoutId', {
@@ -47,54 +35,6 @@ firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, cfpL
     controller: 'PlayingCtrl'
   })
 
-  .state('question-1', {
-    url: "/question-1",
-    templateUrl: "views/template.html",
-    controller: 'Question-1Ctrl'
-  })
-
-  .state('question-2', {
-    url: "/question-2",
-    templateUrl: "views/template.html",
-    controller: 'Question-2Ctrl'
-  })
-
-  .state('question-5', {
-    url: "/question-5",
-    templateUrl: "views/template.html",
-    controller: 'Question-5Ctrl'
-  })
-
-  .state('question-6', {
-    url: "/question-6",
-    templateUrl: "views/template.html",
-    controller: 'Question-6Ctrl'
-  })
-
-  .state('question-7', {
-    url: "/question-7",
-    templateUrl: "views/template.html",
-    controller: 'Question-7Ctrl'
-  })
-
-  .state('question-8', {
-    url: "/question-8",
-    templateUrl: "views/template.html",
-    controller: 'Question-8Ctrl'
-  })
-
-  .state('question-9', {
-    url: "/question-9",
-    templateUrl: "views/template.html",
-    controller: 'Question-9Ctrl'
-  })
-
-  .state('question-10', {
-    url: "/question-10",
-    templateUrl: "views/template.html",
-    controller: 'Question-10Ctrl'
-  })
-
   .state('survey', {
     url: "/survey",
     templateUrl: "views/template.html",
@@ -107,12 +47,11 @@ firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, cfpL
     controller: 'WelcomeCtrl'
   })
 
-  .state('getstarted', {
-    url: "/getstarted",
+  .state('thankyou', {
+    url: "/thankyou",
     templateUrl: "views/template.html",
-    controller: 'GetStartedCtrl'
-  })
-  ;
+    controller: 'ThankYouCtrl'
+  });
 
   $urlRouterProvider.otherwise("/playing");
 
--- js/controllers.js
+++ js/controllers.js
@@ -1,310 +1,162 @@
-  angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngSanitize', 'angular-flexslider', 'cfp.loadingBar'])
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngSanitize', 'angular-flexslider', 'cfp.loadingBar'])
 
-  .controller('IntroCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-    //Used to name the .html file
-    $scope.template = TemplateService.changecontent("intro");
-    $scope.menutitle = NavigationService.makeactive("Intro");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.bgClass = 'intro';
-  })
+.controller('IntroCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+  //Used to name the .html file
+  $scope.template = TemplateService.changecontent("intro");
+  $scope.menutitle = NavigationService.makeactive("Intro");
+  TemplateService.title = $scope.menutitle;
+  $scope.navigation = NavigationService.getnav();
+  $scope.bgColor = "intro";
+})
 
-  .controller('WishCtrl', function($scope, TemplateService, NavigationService, $timeout) {
-    $scope.template = TemplateService.changecontent("wish");
-    $scope.menutitle = NavigationService.makeactive("Wish");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.head = {
-      info: "You just met a genie who grants you four wishes .You choose :"
-    };
-    $scope.wish = [{
-      img: 'img/community.png',
-      name: 'community service'
-    }, {
-      img: 'img/workplace.png',
-      name: 'Happy Workplace'
-    }, {
-      img: 'img/family.png',
-      name: 'Time with Family and Friends'
-    }, {
-      img: 'img/money.png',
-      name: 'Enough Money'
-    }, {
-      img: 'img/independent.png',
-      name: 'Independence'
-    }, {
-      img: 'img/support.png',
-      name: 'support'
-    }, {
-      img: 'img/success.png',
-      name: 'success'
-    }, {
-      img: 'img/recognized.png',
-      name: 'Getting Recognized'
-    }];
+.controller('PlayingCtrl', function($scope, TemplateService, NavigationService, $stateParams, cfpLoadingBar, $state) {
+  $scope.template = TemplateService.changecontent("playing");
+  $scope.menutitle = NavigationService.makeactive("Question");
+  TemplateService.title = $scope.menutitle;
+  $scope.bgColor = "bg-warning";
+  $scope.navigation = NavigationService.getnav();
+  console.log($stateParams.id);
+  var stateParam = "MTUmaHE=";
 
-  })
+  var other = $.jStorage.get("userid");
 
-  .controller('WorkCtrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("work");
-    $scope.menutitle = NavigationService.makeactive("Work");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.head = {
-      info: "You put in your best at work . We know you do .So , your life looks something like this :"
-    };
-    $scope.work = [{
-      img: "img/work/work1.png"
-    }, {
-      img: "img/work/work2.png"
-    }, {
-      img: "img/work/work3.png"
-    }, {
-      img: "img/work/work4.png"
-    }];
+  if ($stateParams.id) {
+    stateParam = $stateParams.id;
+    $.jStorage.set("userid", stateParam);
+  } else if (other) {
+    stateParam = other;
+  }
 
-  })
-
-  .controller('PlayingCtrl', function($scope, TemplateService, NavigationService, $stateParams, cfpLoadingBar, $state) {
-    $scope.template = TemplateService.changecontent("playing");
-    $scope.menutitle = NavigationService.makeactive("Playing");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    console.log($stateParams.id);
-    var stateParam = "MTUmaHE=";
-
-    var other = $.jStorage.get("userid");
-
-    if ($stateParams.id) {
-      stateParam = $stateParams.id;
-      $.jStorage.set("userid", stateParam);
-    } else if (other) {
-      stateParam = other;
+  function populateQuestion(data) {
+    console.log(data);
+    if (data.type == "survey") {
+      $scope.template = TemplateService.changecontent("survey");
+    } else if (data.length > 0) {
+      $scope.template = TemplateService.changecontent("playing");
+    } else if (!data.value) {
+      $scope.template = TemplateService.changecontent("intro");
+      $scope.menutitle = NavigationService.makeactive("intro");
+      TemplateService.title = $scope.menutitle;
+      $scope.navigation = NavigationService.getnav();
+      $scope.bgClass = 'intro';
     }
 
-    function populateQuestion(data) {
-      console.log(data);
-      if (data.type == "survey") {
-        $scope.template = TemplateService.changecontent("survey");
-      } else if (data.length > 0) {
-        $scope.template = TemplateService.changecontent("playing");
-      } else if (!data.value) {
-        $scope.template = TemplateService.changecontent("intro");
-        $scope.menutitle = NavigationService.makeactive("intro");
-        TemplateService.title = $scope.menutitle;
-        $scope.navigation = NavigationService.getnav();
-        $scope.bgClass = 'intro';
-      }
-
-      $scope.allQuestions = data;
-      _.map($scope.allQuestions.questions, function(n) {
-        if (n.type == "3") {
-          n.value = [];
-          _.each(n.option, function(m) {
-            n.value.push(false);
-          });
-        }
-        return n;
-      });
-      $scope.questionIndex = 0;
-      $scope.playing = data[0];
-    }
-
-    NavigationService.pingHq(stateParam, populateQuestion);
-
-    $scope.submitSurvey = function(surveyId, form) {
-
-      var form2 = _.cloneDeep(form);
-
-      var values = _.map(form2, function(n) {
-        var obj = {};
-        if (n.type == "3") {
-          var texts = [];
-          _.each(n.option, function(m, key) {
-            if (n.value[key]) {
-              texts.push(m.title);
-            }
-          });
-          obj.answer = texts.join(',');
-          obj.questionid = n.id;
-        } else {
-          obj.answer = n.value;
-          obj.questionid = n.id;
-        }
-
-        return obj;
-      });
-
-
-      console.log(values);
-      var value2 = _.filter(values, {
-        answer: undefined
-      });
-
-      if (value2.length === 0 ) {
-        NavigationService.saveSurvey(stateParam, surveyId, values, function(data) {
-          console.log(data);
-          NavigationService.pingHq(stateParam, populateQuestion);
+    $scope.allQuestions = data;
+    _.map($scope.allQuestions.questions, function(n) {
+      if (n.type == "3") {
+        n.value = [];
+        _.each(n.option, function(m) {
+          n.value.push(false);
         });
       }
+      return n;
+    });
+    $scope.questionIndex = 0;
+    $scope.playing = data[0];
+  }
 
-    };
-    $scope.selectOption = function(play) {
-      if (play.active === false || !play.active) {
-        play.active = true;
+  NavigationService.pingHq(stateParam, populateQuestion);
+
+  $scope.submitSurvey = function(surveyId, form) {
+
+    var form2 = _.cloneDeep(form);
+
+    var values = _.map(form2, function(n) {
+      var obj = {};
+      if (n.type == "3") {
+        var texts = [];
+        _.each(n.option, function(m, key) {
+          if (n.value[key]) {
+            texts.push(m.title);
+          }
+        });
+        obj.answer = texts.join(',');
+        obj.questionid = n.id;
       } else {
-        play.active = false;
+        obj.answer = n.value;
+        obj.questionid = n.id;
       }
-      cfpLoadingBar.start();
+
+      return obj;
+    });
 
 
+    console.log(values);
+    var value2 = _.filter(values, {
+      answer: undefined
+    });
 
-      NavigationService.saveAnswer(stateParam, $scope.playing.question, play.id, $scope.playing.test, function(data) {
-        // if (data != "true") {
-        //   $scope.questionIndex = 0;
-        // }
-        // if ($scope.questionIndex < $scope.allQuestions.length - 1) {
-        //   $scope.questionIndex++;
-        //   $scope.playing = $scope.allQuestions[$scope.questionIndex];
-        // } else {
-        //   $state.go('intro');
-        // }
-
+    if (value2.length === 0) {
+      NavigationService.saveSurvey(stateParam, surveyId, values, function(data) {
+        console.log(data);
         NavigationService.pingHq(stateParam, populateQuestion);
-
-
-        cfpLoadingBar.complete();
-
       });
+    }
 
-    };
+  };
+  $scope.selectOption = function(play) {
+    if (play.active === false || !play.active) {
+      play.active = true;
+    } else {
+      play.active = false;
+    }
+    cfpLoadingBar.start();
 
-  })
 
-  .controller('Question-2Ctrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("question-2");
-    $scope.menutitle = NavigationService.makeactive("Question-2");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.head = {
-      info: 'How are you feeling today ?'
-    };
-    $scope.qts2 = [{
-      img: "img/qts2/1.png"
-    }, {
-      img: "img/qts2/2.png"
-    }, {
-      img: "img/qts2/3.png"
-    }, {
-      img: "img/qts2/4.png"
-    }, {
-      img: "img/qts2/5.png"
-    }, ];
 
-  })
+    NavigationService.saveAnswer(stateParam, $scope.playing.question, play.id, $scope.playing.test, function(data) {
+      // if (data != "true") {
+      //   $scope.questionIndex = 0;
+      // }
+      // if ($scope.questionIndex < $scope.allQuestions.length - 1) {
+      //   $scope.questionIndex++;
+      //   $scope.playing = $scope.allQuestions[$scope.questionIndex];
+      // } else {
+      //   $state.go('intro');
+      // }
 
-  .controller('Question-1Ctrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("question-1");
-    $scope.menutitle = NavigationService.makeactive("Question-1");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.head = {
-      info: "You have managed to get the much awaited annual leave with family .Your vacation goes something like this :"
-    };
-    $scope.vacation = [{
-      img: "img/vacation/vac1.png"
-    }, {
-      img: "img/vacation/vac2.png"
-    }, {
-      img: "img/vacation/vac3.png"
-    }, {
-      img: "img/vacation/vac4.png"
-    }];
-  })
+      NavigationService.pingHq(stateParam, populateQuestion);
 
-  .controller('Question-5Ctrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("question-5");
-    $scope.menutitle = NavigationService.makeactive("Question-5");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.head = {
-      //            info: "You meet a school friend after a long time and he asks you , + "
-      //            "How's work? "
-      //            " +You say :"
-    };
-  })
 
-  .controller('Question-6Ctrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("question-6");
-    $scope.menutitle = NavigationService.makeactive("Question-6");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.head = {
-      info: "You are in the middle of a project and things aren't going your way . What next ?"
-    };
-  })
+      cfpLoadingBar.complete();
 
-  .controller('Question-7Ctrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("question-7");
-    $scope.menutitle = NavigationService.makeactive("Question-7");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.head = {
-      info: "Your friend approaches you for a job in your company . You are most likely to say this :"
-    };
-  })
+    });
 
-  .controller('Question-8Ctrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("question-8");
-    $scope.menutitle = NavigationService.makeactive("Question-8");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.head = {
-      info: "Congratulations ! Your organization just won an award :"
-    };
-  })
+  };
 
-  .controller('Question-9Ctrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("question-9");
-    $scope.menutitle = NavigationService.makeactive("Question-9");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.head = {
-      info: "Some of the things that you value in life are :"
-    };
-  })
+})
 
-  .controller('Question-10Ctrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("question-10");
-    $scope.menutitle = NavigationService.makeactive("Question-10");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-    $scope.head = {
-      info: "You find yourself smiling because you are blessed with :"
-    };
-  })
+.controller('SurveyCtrl', function($scope, TemplateService, NavigationService) {
+  $scope.template = TemplateService.changecontent("survey");
+  $scope.menutitle = NavigationService.makeactive("Survey");
+  TemplateService.title = $scope.menutitle;
+  $scope.navigation = NavigationService.getnav();
+  $scope.bgColor = "bg-warning";
+})
 
-  .controller('SurveyCtrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("survey");
-    $scope.menutitle = NavigationService.makeactive("Survey");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-  })
+.controller('WelcomeCtrl', function($scope, TemplateService, NavigationService) {
+  $scope.template = TemplateService.changecontent("welcome");
+  $scope.menutitle = NavigationService.makeactive("Welcome");
+  TemplateService.title = $scope.menutitle;
+  TemplateService.header = "";
+  TemplateService.footer = "";
+  $scope.navigation = NavigationService.getnav();
+  $scope.bgColor = "bg-green";
+})
 
-  .controller('WelcomeCtrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("welcome");
-    $scope.menutitle = NavigationService.makeactive("Welcome");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-  })
+.controller('ThankYouCtrl', function($scope, TemplateService, NavigationService) {
+  $scope.template = TemplateService.changecontent("thankyou");
+  $scope.menutitle = NavigationService.makeactive("Thank You");
+  TemplateService.title = $scope.menutitle;
+  TemplateService.header = "";
+  TemplateService.footer = "";
+  $scope.navigation = NavigationService.getnav();
+  $scope.bgColor = "bg-green";
+})
 
-  .controller('GetStartedCtrl', function($scope, TemplateService, NavigationService) {
-    $scope.template = TemplateService.changecontent("getstarted");
-    $scope.menutitle = NavigationService.makeactive("Lets Get Started");
-    TemplateService.title = $scope.menutitle;
-    $scope.navigation = NavigationService.getnav();
-  })
-
-  .controller('headerctrl', function($scope, TemplateService) {
-    $scope.template = TemplateService;
+.controller('headerctrl', function($scope, TemplateService) {
+  $scope.template = TemplateService;
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    $(window).scrollTop(0);
   });
+});
