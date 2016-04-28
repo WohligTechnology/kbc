3f0b0a965982c865dd953458a59f8545e1800862
TusharSachde <tushar@wohlig.com>
2016-04-28 14:32:32 +0530

    Solid Lag Gaye Jagruti ne git nai kiya hai

--- js/app.js
+++ js/app.js
@@ -78,3 +78,23 @@ firstapp.directive("minHeight", function($window) {
     });
   };
 });
+
+firstapp.directive('img', function($compile, $parse) {
+  return {
+    restrict: 'E',
+    replace: false,
+    link: function($scope, element, attrs) {
+      var $element = $(element);
+      if (!attrs.noloading) {
+        $element.after("<img src='img/loading.gif' class='loading' />");
+        var $loading = $element.next(".loading");
+        $element.load(function() {
+          $loading.remove();
+          $(this).addClass("doneLoading");
+        });
+      } else {
+        $($element).addClass("doneLoading");
+      }
+    }
+  };
+});
